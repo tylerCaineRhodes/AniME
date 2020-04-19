f79b86a7b20973fe8a36e0b45fc5e715c7322cab
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-04-19 15:09:54 -0500

    Remove console logs and notes

--- App.js
+++ App.js
@@ -1,32 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView} from 'react-native';
-import {Header, SearchBar, Overlay, Image, Icon} from 'react-native-elements';
-import {Banner, Button, ThemeProvider} from 'react-native-paper';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { Header, SearchBar, Overlay, Image, Icon } from 'react-native-elements';
+import { Banner, Button, ThemeProvider } from 'react-native-paper';
 import Axios from 'axios';
-import ListItem from './ListItem.js';
-import ItemInfo from './ItemInfo.js';
-import SavedItem from './SavedItem';
+import ListItem from './client/components/ListItem.js';
+import ItemInfo from './client/components/ItemInfo.js';
+import SavedItem from './client/components/SavedItem';
 import Logo from './assets/animelogo_opt1.png';
 import AppLogo from './assets/animeicon_480.png';
 import Classics from './assets/animecollage1.jpg';
 import Newies from './assets/animecollage2.png';
 import Pikachu from './assets/pikachuWithHat.png';
-//<Image source={Classics} style={{width:'100%', height: 750}} />
-//<Image source={Pikachu} style={{width: 400, height: 400}} />
+{/* <Image source={Classics} style={{width:'100%', height: 750}} />
+<Image source={Pikachu} style={{width: 400, height: 400}} /> */}
 export default class App extends React.Component {
-
-  constructor(props){
-    super(props)
-    this.state ={
+  constructor(props) {
+    super(props);
+    this.state = {
       search: '',
       data: [],
       savedList: [],
       currentAnime: {},
       listShow: false,
       visible: false,
-      infoIsVisible: false
-
-    }
+      infoIsVisible: false,
+    };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.requestAnime = this.requestAnime.bind(this);
@@ -38,215 +36,287 @@ export default class App extends React.Component {
     this.deleteAnime = this.deleteAnime.bind(this);
   }
 
-  componentDidMount(){
-    this.fetchUserList()
-  }
-  fetchUserList(){
-    Axios.get('http://localhost:3030/getUserList')
-    .then(response => {
-      // console.log('here is the user data from mysql -->', response.data);
-      this.setState({
-        savedList: response.data
-      })
-    })
-    .catch(err => {
-      console.log('problem with fetching data dawg', err)
-    })
+  componentDidMount() {
+    this.fetchUserList();
   }
 
-  requestAnime(uniqueId){
+  fetchUserList() {
+    Axios.get('http://localhost:3030/getUserList')
+      .then((response) => {
+        this.setState({
+          savedList: response.data,
+        });
+      })
+      .catch((err) => {
+        console.log('problem with fetching data dawg', err);
+      });
+  }
+
+  requestAnime(uniqueId) {
     Axios.get(`https://api.jikan.moe/v3/anime/${uniqueId}`)
       .then((response) => {
-      let animeObj = {};
-      animeObj = {'synopsis': response.data['synopsis'], 'title': response.data['title'], 'title_Japanese': response.data['title_japanese'], 'url': response.data['image_url'], 'type': response.data['type'], 'mal_id':response.data['mal_id']}
-     
-      this.setState({
-        currentAnime: animeObj,
-        infoIsVisible: true,
-        listShow: false,
+        let animeObj = {
+          synopsis: response.data['synopsis'],
+          title: response.data['title'],
+          title_Japanese: response.data['title_japanese'],
+          url: response.data['image_url'],
+          type: response.data['type'],
+          mal_id: response.data['mal_id'],
+        };
+
+        this.setState({
+          currentAnime: animeObj,
+          infoIsVisible: true,
+          listShow: false,
+        });
       })
-    })
-    .catch(err => {
-      console.log('nah, dude from request anime', err)
-    })
+      .catch((err) => {
+        console.log('nah, dude from request anime', err);
+      });
   }
-  
-  handleGoHome(){
+
+  handleGoHome() {
     this.setState({
       infoIsVisible: false,
-      listShow: false
-    })
-  }
-  goToHomeScreen(){
-    this.setState({
-      data: []
-    })
-
-  }
-  showList(){
-    this.setState({
-      listShow: true
-    })
+      listShow: false,
+    });
   }
 
-  postNewItem(response){
+  goToHomeScreen() {
+    this.setState({
+      data: [],
+    });
+  }
+
+  showList() {
+    this.setState({
+      listShow: true,
+    });
+  }
+
+  postNewItem(response) {
     Axios.post('http://localhost:3030/postNewItem', {
-      'synopsis':response.data['synopsis'], 
-      'title':response.data['title'], 
-      'title_Japanese':response.data['title_japanese'], 
-      'url':response.data['image_url'], 
-      'type':response.data['type'], 
-      'mal_id':response.data['mal_id'], 
-      'episodes':response.data['episodes'], 
-      'rating':response.data['rating']
+      synopsis: response.data['synopsis'],
+      title: response.data['title'],
+      title_Japanese: response.data['title_japanese'],
+      url: response.data['image_url'],
+      type: response.data['type'],
+      mal_id: response.data['mal_id'],
+      episodes: response.data['episodes'],
+      rating: response.data['rating'],
     })
-    .then(() => {
-      this.fetchUserList()
-    })
-    .catch((err) => {
-      console.log('didn\'t work when sending from axios', err)
-    })
-  }
-  deleteAnime(uniqueId){
-    Axios.delete('http://localhost:3030/deleteAnime', {
-      params: {uniqueId}
-    })
-    .then(() => {
-      console.log('succesfully deleted!')
-      this.fetchUserList()
-    })
-    .catch((err) => {
-      console.log('couldn\'t delete from front', err)
-    })
+      .then(() => {
+        this.fetchUserList();
+      })
+      .catch((err) => {
+        console.log("didn't work when sending from axios", err);
+      });
   }
 
-  addToList(uniqueId){
+  deleteAnime(uniqueId) {
+    Axios.delete('http://localhost:3030/deleteAnime', {
+      params: { uniqueId },
+    })
+      .then(() => {
+        console.log('succesfully deleted!');
+        this.fetchUserList();
+      })
+      .catch((err) => {
+        console.log("couldn't delete from front", err);
+      });
+  }
+
+  addToList(uniqueId) {
     Axios.get(`https://api.jikan.moe/v3/anime/${uniqueId}`)
       .then((response) => {
-      let animeObj = {};
-      // console.log('response object ---> ', response.data)
-      this.postNewItem(response)
-    })
-    .then(() => {
-      this.setState({
-        infoIsVisible: false
+        this.postNewItem(response);
       })
-    })
-    .catch(err => {
-      console.log('nah, dude', err)
-    }) 
+      .then(() => {
+        this.setState({
+          infoIsVisible: false,
+        });
+      })
+      .catch((err) => {
+        console.log('nah, dude', err);
+      });
   }
 
-  handleChange(search){
-      this.setState({search})
-   };
+  handleChange(search) {
+    this.setState({ search });
+  }
 
-  handleSubmit(){
-    Axios.get( `https://api.jikan.moe/v3/search/anime?q=${this.state.search}&limit=10`)
-    .then((response) => {
-      let result = [];
+  handleSubmit() {
+    Axios.get(`https://api.jikan.moe/v3/search/anime?q=${this.state.search}&limit=10`)
+      .then((response) => {
+        let result = [];
 
-      for(var keys in response.data){
-        result.push(response.data[keys])
-      }
-      this.setState({
-        data: result[3],
-        search: ''
+        for (var keys in response.data) {
+          result.push(response.data[keys]);
+        }
+        this.setState({
+          data: result[3],
+          search: '',
+        });
       })
-    })
-    .catch(err => {
-      console.log('nah, dude')
-    })
-  };
+      .catch((err) => {
+        console.log('nah, dude');
+      });
+  }
 
-  render(){
-    const opener = <Image source={Classics} style={{width:'100%', height: 750}} />
-    const nothing = <Text>""</Text>
+  render() {
+    const opener = (
+      <Image source={Classics} style={{ width: '100%', height: 750 }} />
+    );
+    const nothing = <Text>""</Text>;
     return (
       <>
-      <Header
-    leftComponent={{ icon: 'home', color: '#fff', onPress: () => {this.goToHomeScreen()}}}
-    centerComponent={<Image source={Logo} style={{width:150, height: 50, marginBottom: 20}} />}
-    rightComponent={{ icon: 'list', color: '#fff', onPress: () => {this.showList()}}}
-    containerStyle={{
-      backgroundColor: '#3D4AA3',
-      justifyContent: 'space-around',
-    }}
-    />
+        <Header
+          leftComponent={{
+            icon: 'home',
+            color: '#fff',
+            onPress: () => {
+              this.goToHomeScreen();
+            },
+          }}
+          centerComponent={
+            <Image
+              source={Logo}
+              style={{ width: 150, height: 50, marginBottom: 20 }}
+            />
+          }
+          rightComponent={{
+            icon: 'list',
+            color: '#fff',
+            onPress: () => {
+              this.showList();
+            },
+          }}
+          containerStyle={{
+            backgroundColor: '#3D4AA3',
+            justifyContent: 'space-around',
+          }}
+        />
 
-    <Overlay isVisible={this.state.infoIsVisible} fullScreen={false} borderRadius={20} width={400} containerStyle={styles.modal}>
-      <ItemInfo itemId={this.state.currentAnime.mal_id} description={this.state.currentAnime.synopsis} title={this.state.currentAnime.title} title_japanese={this.state.currentAnime.title_Japanese} image={this.state.currentAnime.url} type={this.state.currentAnime.type} handleGoHome={this.handleGoHome} addToList={this.addToList}/>
-    </Overlay>
+        <Overlay
+          isVisible={this.state.infoIsVisible}
+          fullScreen={false}
+          borderRadius={20}
+          width={400}
+          containerStyle={styles.modal}
+        >
+          <ItemInfo
+            itemId={this.state.currentAnime.mal_id}
+            description={this.state.currentAnime.synopsis}
+            title={this.state.currentAnime.title}
+            title_japanese={this.state.currentAnime.title_Japanese}
+            image={this.state.currentAnime.url}
+            type={this.state.currentAnime.type}
+            handleGoHome={this.handleGoHome}
+            addToList={this.addToList}
+          />
+        </Overlay>
 
-    <Overlay isVisible={this.state.listShow} fullScreen={false} borderRadius={20} width={400} containerStyle={styles.modal}>
-    <ScrollView>
-    {this.state.savedList.map((item, i) => {
-      return (
-        <SavedItem key={i} image={item.url} title={item.title} description={item.synopsis} itemId={item.mal_id} requestAnime={this.requestAnime} deleteAnime={this.deleteAnime} />
-      )
-      })}
-      <Button mode="contained" style={styles.bigButton} onPress={() => this.handleGoHome()} color={'#3D4AA3'}>Homミ</Button>
-      </ScrollView>
-   </Overlay> 
+        <Overlay
+          isVisible={this.state.listShow}
+          fullScreen={false}
+          borderRadius={20}
+          width={400}
+          containerStyle={styles.modal}
+        >
+          <ScrollView>
+            {this.state.savedList.map((item, i) => {
+              return (
+                <SavedItem
+                  key={i}
+                  image={item.url}
+                  title={item.title}
+                  description={item.synopsis}
+                  itemId={item.mal_id}
+                  requestAnime={this.requestAnime}
+                  deleteAnime={this.deleteAnime}
+                />
+              );
+            })}
+            <Button
+              mode="contained"
+              style={styles.bigButton}
+              onPress={() => this.handleGoHome()}
+              color={'#3D4AA3'}
+            >
+              Homミ
+            </Button>
+          </ScrollView>
+        </Overlay>
 
-    <Banner 
-      style= {styles.banner}
-      contentStyle={styles.links}
-      visible={this.state.visible} 
-      actions={[
-        {
-          label: 'profile',
-          onPress: () => this.setState({ visible: false }),
-        },
-        {
-          label: 'settings',
-          onPress: () => this.setState({ listShow: true}),
-        },
-        {
-          label: 'friends',
-          onPress: () => this.setState({ listShow: true}),
-        },
-        {
-          label: 'list',
-          onPress: () => this.setState({ listShow: true}),
-        },
-      ]}
-      // icon={({ size }) =>
-      //     <Image
-      //       source={Pikachu}
-      //       style={{
-      //         width: 10,
-      //         height: 20,
-      //       }}
-      //     />
-      //   }
-      >
-     </Banner>
+        <Banner
+          style={styles.banner}
+          contentStyle={styles.links}
+          visible={this.state.visible}
+          actions={[
+            {
+              label: 'profile',
+              onPress: () => this.setState({ visible: false }),
+            },
+            {
+              label: 'settings',
+              onPress: () => this.setState({ listShow: true }),
+            },
+            {
+              label: 'friends',
+              onPress: () => this.setState({ listShow: true }),
+            },
+            {
+              label: 'list',
+              onPress: () => this.setState({ listShow: true }),
+            },
+          ]}
+          // icon={({ size }) =>
+          //     <Image
+          //       source={Pikachu}
+          //       style={{
+          //         width: 10,
+          //         height: 20,
+          //       }}
+          //     />
+          //   }
+        ></Banner>
 
-      <SearchBar
-        value={this.state.search}
-        onChangeText={this.handleChange}
-        platform={'ios'}
-        round={true}
-        showCancel={false}
-        showLoading={true}
-        returnKeyType = {'search'}
-      />
+        <SearchBar
+          value={this.state.search}
+          onChangeText={this.handleChange}
+          platform={'ios'}
+          round={true}
+          showCancel={false}
+          showLoading={true}
+          returnKeyType={'search'}
+        />
 
-        <Button mode="contained" style={styles.bigButton} onPress={() => this.handleSubmit()} color={'#3D4AA3'}>Sミarch</Button>
+        <Button
+          mode="contained"
+          style={styles.bigButton}
+          onPress={() => this.handleSubmit()}
+          color={'#3D4AA3'}
+        >
+          Sミarch
+        </Button>
         <ScrollView>
-
           {this.state.data.map((item, i) => {
-            // console.log('current item -->', item)
             return (
-              <ListItem key={i} image={item.image_url} title={item.title} description={item.synopsis} itemId={item.mal_id} requestAnime={this.requestAnime} />
-            )
+              <ListItem
+                key={i}
+                image={item.image_url}
+                title={item.title}
+                description={item.synopsis}
+                itemId={item.mal_id}
+                requestAnime={this.requestAnime}
+              />
+            );
           })}
-      
-        <View style={styles.message}>
-        {/* <Text style={styles.textHello}>Search for your favorite anime!</Text> */}
-        {this.state.data.length === 0 ? opener : nothing}
-        </View>
+
+          <View style={styles.message}>
+            {/* <Text style={styles.textHello}>Search for your favorite anime!</Text> */}
+            {this.state.data.length === 0 ? opener : nothing}
+          </View>
         </ScrollView>
       </>
     );
@@ -261,27 +331,26 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   SearchBar: {
-    backgroundColor:'grey'
+    backgroundColor: 'grey',
   },
   message: {
     // top: 200,
     // left: 11,
-    overflow: 'scroll'
+    overflow: 'scroll',
   },
   textHello: {
     fontWeight: 'bold',
     left: 50,
-    fontSize: 30
+    fontSize: 30,
   },
   bigButton: {
     marginHorizontal: 2,
-    borderRadius: 15
+    borderRadius: 15,
   },
   links: {
-    left: 0
+    left: 0,
   },
-  banner:{
+  banner: {
     opacity: 1.5,
-   
   },
 });
---
+++ client/components/ItemInfo.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import { Button } from 'react-native-elements';
+
+export default function ItemInfo({
+  description,
+  title,
+  title_japanese,
+  image,
+  type,
+  handleGoHome,
+  itemId,
+  addToList,
+}) {
+  
+  return (
+    <ScrollView>
+      <View style={styles.item}>
+        <Image source={{ uri: image }} style={{ width: 250, height: 250 }} />
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.title}>{title_japanese}</Text>
+        <Text style={styles.description}>{description}</Text>
+        <View style={styles.buttonLine}>
+          <Button
+            title="home 帰る"
+            buttonStyle={styles.button}
+            onPress={() => {
+              handleGoHome();
+            }}
+          />
+          <Button
+            title="Add to list! セーブデータ"
+            buttonStyle={styles.button}
+            onPress={() => {
+              addToList(itemId);
+            }}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  item: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    overflow: 'scroll',
+  },
+  title: {
+    fontWeight: 'bold',
+    position: 'relative',
+  },
+  description: {
+    width: 300,
+  },
+  buttonLine: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  button: {
+    margin: 10,
+    backgroundColor: '#3D4AA3',
+    borderRadius: 5,
+  },
+});
