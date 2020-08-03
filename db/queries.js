22641a14b2ad0d0c6dcc24e174390f16fb58e432
Tyler Rhodes <tyler.rhodes@yale.edu>
2020-08-02 22:18:40 -0500

    Get rid of duplication in junction select query

--- db/queries.js
+++ db/queries.js
@@ -8,13 +8,49 @@ const connection = mysql.createConnection({
   database: process.env.DB_NAME,
 });
 
-const getList = (userId) => {
+const getSavedList = (userId) => {
   return new Promise((resolve, reject) => {
-    const query = `select l.title, l.title_japanese, l.url, l.type, l.mal_id, l.episodes, l.rating from savedlist l inner join users_savedlist us on (l.mal_id = us.anime_id) inner join users u on (u.id = us.user_id) where u.id = ${userId};`;
+    const input = [userId];
+    const query = `SELECT DISTINCT l.title, l.title_japanese, l.url, l.type, l.mal_id, l.episodes, l.rating FROM savedlist l 
+      INNER JOIN users_savedlist us ON (l.mal_id = us.anime_id)
+      INNER JOIN users u ON (u.id = us.user_id) WHERE u.id = ? ;`;
 
-    connection.query(query, (err, data) => {
+    connection.query(query, input, (err, data) => {
       if (err) {
-        console.log('something went wrong with fetching from list in db', err);
+        reject(err);
+      } else {
+        resolve(data);
+      };
+    });
+  });
+};
+
+const postAnime = (anime) => {
+  return new Promise((resolve, reject) => {
+    const { title, title_Japanese, url, type, episodes, mal_id, rating, userId } = anime;
+    const input = [ title.trim(), title_Japanese.trim(), url.trim(), type.trim(), episodes, mal_id, rating ];
+    const query = `INSET INTO savedlist(title, title_Japanese, url, type, episodes, mal_id, rating) VALUES (?,?,?,?,?,?,?)`;
+
+    connection.query(query, input, (err, data) => {
+      if (err) {
+        //if anime already exists in the db, add add new anime-username pair to junction table
+        addToJunction(mal_id, userId)
+          .then((data) => resolve(data))
+          .catch((error) => reject(error));
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+const getUser = async (user) => {
+  return new Promise((resolve, reject) => {
+    const query = `SELECT * FROM users WHERE username = ?`;
+    const input = [user];
+
+    connection.query(query, input, (err, data) => {
+      if (err) {
         reject(err);
       } else {
         resolve(data);
@@ -23,88 +59,47 @@ const getList = (userId) => {
   });
 };
 
-const postAnime = (anime) => {
+const addUser = async (username, password) => {
   return new Promise((resolve, reject) => {
-    const {title, title_Japanese, url, type, episodes, mal_id, rating} = anime;
-    const query = `insert into savedlist(title, title_Japanese, url, type, episodes, mal_id, rating) values (?,?,?,?,?,?,?)`;
-    const input = [ title.trim(), title_Japanese.trim(), url.trim(), type.trim(), episodes, mal_id, rating ];
-
-    connection.query(query, input, (err, data) => {
-        if (err) {
-          console.log('problem with adding object to mysql table from db');
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      }
-    );
-  });
-};
-
-const getUser = async(user) => {
-  console.log(user, '<-- username')
-  return new Promise((resolve, reject) => {
-    const query = `select * from users where username = ?`
-    const input = [user];
-
-    connection.query(query, input, (err, data) => {
-      if(err){
-        console.log('err from query')
-        reject(err)
-      } else {
-        resolve(data)
-      }
-    })
-  })
-}
-
-const addUser = async(username, password) => {
-  return new Promise((resolve, reject) => {
-    const query = `insert into users (username, password) values (?,?)`
+    const query = `INSERT INTO users (username, password) VALUES (?,?)`;
     const input = [username, password];
 
     connection.query(query, input, (err, data) => {
-      if(err){
-        console.log('err from query')
-        reject(err)
+      if (err) {
+        reject(err);
       } else {
-        resolve(data)
+        resolve(data);
       }
-    })
-  })
-}
+    });
+  });
+};
 
 const deleteAnime = (args) => {
   const { mal_id, user_id } = args;
-  const query = `delete from users_savedlist where (anime_id =${mal_id} and user_id=${user_id});`;
+  const query = `DELETE FROM users_savedlist WHERE (anime_id =${mal_id} AND user_id=${user_id}) LIMIT 1;`;
   return new Promise((resolve, reject) => {
     connection.query(query, (err, data) => {
-        if (err) {
-          console.log('whoops in deleting a row from junction table');
-          reject(err);
-        } else {
-          resolve(data);
-        }
-      }
-    );
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      };
+    });
   });
 };
 
 const addToJunction = (mal_id, userId) => {
-  const query = `insert into users_savedlist (anime_id, user_id) values (?,?)`;
+  const query = `INSERT INTO users_savedlist (anime_id, user_id) VALUES (?,?)`;
   const input = [mal_id, userId];
 
   return new Promise((resolve, reject) => {
     connection.query(query, input, (err, data) => {
-        if (err) {
-          console.log('whoops in posting a thing into junction table');
-          reject(err);
-        } else {
-          console.log('data from users_savedlist pass')
-          resolve(data);
-        }
-      }
-    );
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      };
+    });
   });
 };
 
@@ -113,7 +108,14 @@ connection.connect((err) => {
     console.log("can't connected to myslq");
   } else {
     console.log("you're connected to your database");
-  }
+  };
 });
 
-module.exports = { getList, postAnime, deleteAnime, getUser, addUser, addToJunction };
+module.exports = {
+  getSavedList,
+  postAnime,
+  deleteAnime,
+  getUser,
+  addUser,
+  addToJunction,
+};
